a6891277de5778e65c397ec5c584c1f0cc27b2c9
Shekhar Giri <261085in@gmail.com>
2026-07-29 20:22:48 +0530

    initial commit

---
+++ src/main/java/com/giri/ai/banking_faq_assistant/BankingFaqAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.giri.ai.banking_faq_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingFaqAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingFaqAssistantApplication.class, args);
+	}
+
+}
